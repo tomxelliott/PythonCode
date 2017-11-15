18831d7e5c4d66a64fb314931fc12f9d8df39197
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-11-15 23:00:46 +0000

    Update binary_search.py

--- src/binary_search.py
+++ src/binary_search.py
@@ -10,6 +10,10 @@ def binary_search(data, target, low, high):
         else:
             return binary_search(data, target, mid+1, high)
 
-x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-user_input = int(input())
-print binary_search(x, user_input, x[0], x[-1])
+def main():
+    x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    user_input = int(input())
+    print binary_search(x, user_input, x[0], x[-1])
+
+if __name__=='__main__':
+    main()
