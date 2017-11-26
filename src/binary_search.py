62eb0276c3958ea6fe5b27c5ef4cfb593de208aa
Tom Elliott <tomxelliott@hotmail.co.uk>
2017-11-26 17:49:51 +0000

    Update binary_search.py

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,12 +1,10 @@
 import sys
 
-def binary_search(data, target, low, high):
+def binary_search(target, low, high):
     if target > high:
         print "too big dude!"
     elif target < low:
         print "too small dude! ;p"
-    elif not isinstance(target, int):
-        print "the target we are searching for must be a number!"
     else:
         if low > high:
             return False
@@ -14,35 +12,35 @@ def binary_search(data, target, low, high):
             mid = (low+high)//2
             if target == mid:
                 return True
-            elif target < data[mid]:
-                return binary_search(data, target, low, mid-1)
+            elif target < mid:
+                return binary_search(target, low, mid-1)
             else:
-                return binary_search(data, target, mid+1, high)
+                return binary_search(target, mid+1, high)
 
 def main():
     try:
-        x = []
-        i = 0
-        while i < 20:
-            try:
-                j = int(input())
-                if j < 0 or not isinstance(j, int):
-                    raise ValueError
-                x.append(j)
-                i += 1
-            except ValueError:
-                print "Please enter a positive number."
-            except NameError:
-                print "Please enter a valid number, not any other type!"
-        x.sort()
-        print x
-        print ""
-        print "Please enter a number you would like to search..."
-        user_input = input()
-        print binary_search(x, user_input, x[0], x[-1])
+        print "Please enter a low value: "
+        lower = int(input())
+
+        print "Please enter a high value: "
+        higher = int(input())
+
+        if lower and higher:
+            print "Please enter a number you would like to search..."
+            user_input = int(input())
+            print binary_search(user_input, lower, higher)
+        else:
+            print "Please try again..."
+    except ValueError:
+        print "Please enter a positive number."
+    except NameError:
+        print "Please enter a valid number, not any other type!"
+    except SyntaxError:
+        print "Please enter a valid number!!!"
+    except UnboundLocalError:
+        print "Low value and High value not entered correctly!"
     except KeyboardInterrupt:
         sys.exit()
 
 if __name__=='__main__':
     main()
-
