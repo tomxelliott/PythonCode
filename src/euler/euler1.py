27c47415c78c01b100ea2a38cb2dc574db5feb2f
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-02-18 20:36:36 +0000

    Update euler1.py

--- src/euler/euler1.py
+++ src/euler/euler1.py
@@ -1,2 +1,6 @@
-result = sum(i for i in range(1000) if (i % 3 == 0 or i % 5 == 0))
-print result
+def main():
+  result = sum(i for i in range(1000) if (i % 3 == 0 or i % 5 == 0))
+  print result
+
+if __name__ == '__main__':
+	main()
